68e3be7284ebd18d91b55f058cd51fa69f0b253e
kimdk121 <kimdk121@naver.com>
2025-01-06 19:53:47 +0900

    .
    .

--- springmvc/src/main/java/hello/springmvc/basic/requestmapping/MappingClassController.java
+++ springmvc/src/main/java/hello/springmvc/basic/requestmapping/MappingClassController.java
@@ -1,25 +1,33 @@
 package hello.springmvc.basic.requestmapping;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/mapping/users")
 public class MappingClassController {
 
-    @GetMapping("/mapping/users")
+    @GetMapping
     public String user() {
         return "get users";
     }
 
-    @PostMapping("/mapping/users")
+    @PostMapping
     public String addUser() {
         return "post user";
     }
 
-    @GetMapping("/mapping/users/{userId}")
+    @GetMapping("/{userId}")
     public String findUser(@PathVariable String userId) {
-        
+        return "get userId=" + userId;
+    }
+
+    @PatchMapping("/{userId}")
+    public String updateUser(@PathVariable String userId) {
+        return "update userId=" + userId;
+    }
+
+    @DeleteMapping("/{userId}")
+    public String deleteUser(@PathVariable String userId) {
+        return "delete userId=" + userId;
     }
 }
